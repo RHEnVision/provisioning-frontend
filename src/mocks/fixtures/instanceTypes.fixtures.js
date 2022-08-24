70e153b1ac9c6f258f4754884562ce38fbc42ce5
aabramov <aabramov@redhat.com>
2022-08-24 14:08:35 +0300

    Add instance type info

--- src/mocks/fixtures/instanceTypes.fixtures.js
+++ src/mocks/fixtures/instanceTypes.fixtures.js
@@ -1,6 +1,19 @@
 export const instanceTypeList = [
   {
-    id: 'Instance Type 1',
+    id: 1,
+    name: 'm5dn.12xlarge',
+    vcpus: 48,
+    cores: 24,
+    memory: 196608,
+    supported: true,
+    architecture: 'x86_64',
   },
-  { id: 'Instance Type 2' },
+  {
+    id: 2,
+    name: 't4g.nano',
+    vcpus: 2,
+    cores: 2,
+    memory: 512,
+    architecture: 'arm64',
+  },
 ];
