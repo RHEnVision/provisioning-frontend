0bac04c0bf475c18dd60bd0a419b9a3717071916
Amir Fefer <amirfefer@gmail.com>
2022-08-29 00:32:41 +0300

    Add image selector in demo page

--- src/Routes/SamplePage/SamplePage.js
+++ src/Routes/SamplePage/SamplePage.js
@@ -1,38 +1,96 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import { Button, StackItem, Stack, Title } from '@patternfly/react-core';
+import {
+  Button,
+  StackItem,
+  Stack,
+  Title,
+  Select,
+  SelectOption,
+  SelectVariant,
+} from '@patternfly/react-core';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
 import {
   PageHeader,
   PageHeaderTitle,
 } from '@redhat-cloud-services/frontend-components/PageHeader';
+import axios from 'axios';
 
 import './sample-page.scss';
 import ProvisioningWizard from '../../Components/Wizard';
 
 /**
- * A smart component that handles all the api calls and data needed by the dumb components.
- * Smart components are usually classes.
- *
- * https://reactjs.org/docs/components-and-props.html
- * https://medium.com/@thejasonfile/dumb-components-and-smart-components-e7b33a698d43
+This page demonstrates the provisioning UI wizard
  */
+
+// This default AMI value is taken when no image builder api found
+const DEFAULT_AMI = 'ami-05fa00d4c63e32376';
+const IMAGE_BUILDER_URL = '/api/image-builder/v1/composes';
+
 const SamplePage = () => {
   const [isWizardOpen, setWizardModal] = React.useState(false);
+  const [images, setImages] = React.useState([]);
+  const [chosenImage, setChosenImage] = React.useState({
+    id: undefined,
+    name: undefined,
+  });
+  const [isImageSelectOpen, setImageSelect] = React.useState(false);
 
   useEffect(() => {
+    const fetchImagesFromIB = async () => {
+      try {
+        const { data } = await axios.get(IMAGE_BUILDER_URL);
+        setImages(data?.data);
+      } catch (e) {
+        console.debug('No image builder api has been found');
+        setChosenImage({ id: DEFAULT_AMI, name: 'default-image' });
+      }
+    };
+
     insights?.chrome?.appAction?.('sample-page');
+    fetchImagesFromIB();
   }, []);
+
+  const onImageSelectionToggle = (isOpen) => {
+    setImageSelect(isOpen);
+  };
+
+  const onImageSelect = (event, selection, isPlaceholder) => {
+    if (isPlaceholder) return;
+    const { id } = images.find((image) => image.image_name === selection);
+    setChosenImage({ id, name: selection });
+    setImageSelect(false);
+  };
 
   return (
     <React.Fragment>
       <PageHeader>
-        <PageHeaderTitle title="Sample Insights App" />
+        <PageHeaderTitle title="Sample Provisioning App" />
         <p> Provisioning Demo </p>
       </PageHeader>
       <Main>
         <Stack hasGutter>
+          <StackItem>
+            <Select
+              width="33%"
+              variant={SelectVariant.single}
+              placeholderText="Select an image"
+              aria-label="Select Image"
+              onToggle={onImageSelectionToggle}
+              onSelect={onImageSelect}
+              selections={chosenImage.name}
+              isOpen={isImageSelectOpen}
+            >
+              {images?.map((option) => (
+                <SelectOption
+                  key={option.id}
+                  value={option.image_name}
+                  description={`ID: ${option.id}`}
+                />
+              ))}
+            </Select>
+          </StackItem>
           <StackItem>
             <Title headingLevel="h2" size="3xl"></Title>
             <Button variant="primary" onClick={() => setWizardModal(true)}>
@@ -41,7 +99,7 @@ const SamplePage = () => {
             <ProvisioningWizard
               isOpen={isWizardOpen}
               onClose={() => setWizardModal(false)}
-              image={{ name: 'example-image', id: 'composeID' }}
+              image={chosenImage}
             />
           </StackItem>
         </Stack>
