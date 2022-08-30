8477c674457acd6f6a08a4a02c2d36d7b9b581bb
aabramov <aabramov@redhat.com>
2022-08-30 17:04:59 +0300

    Add region support

--- src/API/queryKeys.js
+++ src/API/queryKeys.js
@@ -1,6 +1,7 @@
 export const SOURCES_QUERY_KEY = 'sources';
 export const PUBKEYS_QUERY_KEY = 'pubkeys';
-export const instanceTypesQueryKeys = (chosenSource) => [
+export const instanceTypesQueryKeys = (source, region) => [
   'instanceTypes',
-  chosenSource,
+  source,
+  region,
 ];
