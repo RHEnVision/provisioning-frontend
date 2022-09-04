b258c4af4a8dce41b17d4470835e6d3115063dda
Amir Fefer <amirfefer@gmail.com>
2022-09-05 00:13:31 +0300

    HMSPROV-166 - clear the wizard on close

--- src/Components/Wizard/index.js
+++ src/Components/Wizard/index.js
@@ -6,18 +6,28 @@ import { WizardProvider } from '../Common/WizardContext';
 import APIProvider from '../Common/Query';
 import defaultSteps from './steps';
 
+const DEFAULT_STEP_VALIDATION = {
+  sshStep: false,
+};
+
 const ProvisioningWizard = ({ isOpen, onClose, image, ...props }) => {
   const [stepIdReached, setStepIdReached] = React.useState(1);
-  const [stepValidation, setStepValidation] = React.useState({
-    sshStep: false,
-  });
+  const [stepValidation, setStepValidation] = React.useState(
+    DEFAULT_STEP_VALIDATION
+  );
+
+  const onCustomClose = () => {
+    setStepIdReached(1);
+    setStepValidation(DEFAULT_STEP_VALIDATION);
+    onClose();
+  };
 
   const steps = defaultSteps({
     stepIdReached,
     image,
     stepValidation,
     setStepValidation,
-    onClose,
+    onClose: onCustomClose,
   });
 
   const onNext = ({ id, name }, { prevId, prevName }) => {
@@ -27,7 +37,7 @@ const ProvisioningWizard = ({ isOpen, onClose, image, ...props }) => {
     setStepIdReached((prevID) => (prevID < id ? id : prevID));
   };
 
-  return (
+  return isOpen ? (
     <WizardProvider>
       <APIProvider>
         <Wizard
@@ -35,13 +45,13 @@ const ProvisioningWizard = ({ isOpen, onClose, image, ...props }) => {
           title="Provisioning"
           description="Provision RHEL images to cloud"
           steps={steps}
-          isOpen={isOpen}
-          onClose={onClose}
+          isOpen
+          onClose={onCustomClose}
           onNext={onNext}
         />
       </APIProvider>
     </WizardProvider>
-  );
+  ) : null;
 };
 
 ProvisioningWizard.propTypes = {
