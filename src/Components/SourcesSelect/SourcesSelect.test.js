a7b4a97190cb46054006696481c611037d8c22d4
Amir Fefer <amirfefer@gmail.com>
2022-12-01 19:38:08 +0200

    fix(HMSPROV-378) - use userEvent over fireEvent

--- src/Components/SourcesSelect/SourcesSelect.test.js
+++ src/Components/SourcesSelect/SourcesSelect.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import SourcesSelect from '.';
+import userEvent from '@testing-library/user-event';
 import { sourcesList } from '../../mocks/fixtures/sources.fixtures';
-import { render, fireEvent, screen } from '../../mocks/utils';
+import { render, screen } from '../../mocks/utils';
 
 describe('SourcesSelect', () => {
   test('preselects the chosen source', async () => {
     render(<SourcesSelect setValidation={jest.fn()} />);
     // wizard context mock is `{chosenSource: 1}`
     const selectDropdown = await screen.findByText('Source 1');
-    fireEvent.click(selectDropdown);
+    await userEvent.click(selectDropdown);
+
     const items = await screen.findAllByLabelText('Source account');
     expect(items).toHaveLength(sourcesList.length);
   });
