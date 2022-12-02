3f80f746df9a5953882db748926619ca3af532b8
Akhil Jha <akjha@redhat.com>
2022-12-02 16:37:38 +0530

    ci: update pr_check

--- .rhcicd/pr_check.sh
+++ .rhcicd/pr_check.sh
@@ -4,7 +4,7 @@
 # Export vars for helper scripts to use
 # --------------------------------------------
 export APP_NAME="provisioning"  # name of app-sre "application" folder this component lives in
-export COMPONENT_NAME="provisioning-backend"  # name of resourceTemplate component for deploy
+export COMPONENT_NAME="provisioning-frontend"  # name of resourceTemplate component for deploy
 # IMAGE should match the quay repo set by app.yaml in app-interface
 export IMAGE="quay.io/cloudservices/provisioning-frontend"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT} # if running in jenkins, use the build's workspace
@@ -15,22 +15,26 @@ COMMON_BUILDER=https://raw.githubusercontent.com/RedHatInsights/insights-fronten
 # --------------------------------------------
 # Options that must be configured by app owner
 # --------------------------------------------
-IQE_PLUGINS="hms-provisioning"
-IQE_MARKER_EXPRESSION="smoke"
-IQE_FILTER_EXPRESSION=""
+export IQE_PLUGINS="provisioning"
+export IQE_MARKER_EXPRESSION="ui and smoke"
+export IQE_FILTER_EXPRESSION=""
+export IQE_ENV="ephemeral"
+export IQE_SELENIUM="true"
+export IQE_CJI_TIMEOUT="30m"
+export DEPLOY_TIMEOUT="900"  # 15min
+export DEPLOY_FRONTENDS="true"
 
 set -exv
 # source is preferred to | bash -s in this case to avoid a subshell
 source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
-BUILD_RESULTS=$?
 
-# Stubbed out for now
-mkdir -p $WORKSPACE/artifacts
-cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+# shellcheck source=/dev/null
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
-# teardown_docker
-exit $BUILD_RESULTS
+# Run ui tests
+source "${CICD_ROOT}/deploy_ephemeral_env.sh"
+export COMPONENT_NAME="provisioning-backend"
+source "${CICD_ROOT}/cji_smoke_test.sh"
+source "${CICD_ROOT}/post_test_results.sh"
