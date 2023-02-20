17a57264aa74379992fbee2cd8e23a9f15b97e12
Ondrej Ezr <oezr@redhat.com>
2023-02-20 10:30:47 +0100

    build: include coverage in sonar scan

--- .rhcicd/sonarqube.sh
+++ .rhcicd/sonarqube.sh
@@ -11,28 +11,28 @@ COMMIT_SHORT=$(git rev-parse --short=7 HEAD)
 # Both ${GIT_BRANCH}  and ${ghprbPullId} are provided by App-Interface's Jenkins.
 # SonarQube parameters can be found below:
 #   https://sonarqube.corp.redhat.com/documentation/analysis/pull-request/
-if [[ "${SONAR_PR_CHECK}" = "true" ]]; then
-    export PR_CHECK_OPTS="-Dsonar.pullrequest.branch=${GIT_BRANCH} -Dsonar.pullrequest.key=${ghprbPullId} -Dsonar.pullrequest.base=main";
+if [ $IS_PR = true ]; then
+    pr_check_ots="-Dsonar.pullrequest.branch=${GIT_BRANCH} -Dsonar.pullrequest.key=${ghprbPullId} -Dsonar.pullrequest.base=main";
 fi
 
-#coverage_opts="-Dsonar.go.tests.reportPaths=/usr/src/coverage.json -Dsonar.go.coverage.reportPaths=/usr/src/coverage.txt"
+coverage_opts="-Dsonar.javascript.lcov.reportPaths=/usr/src/coverage/lcov.info"
 
 podman run \
 --pull=always --rm \
 -v "${PWD}":/usr/src:z   \
 -e SONAR_SCANNER_OPTS="-Dsonar.scm.provider=git \
- ${PR_CHECK_OPTS:-} \
- ${coverage_opts:-}\
+ ${pr_check_ots:-} \
+ ${coverage_opts:-} \
  -Dsonar.working.directory=/tmp \
  -Dsonar.projectKey=hms:provisioning-frontend \
  -Dsonar.projectVersion=${COMMIT_SHORT} \
  -Dsonar.sources=/usr/src/. \
  -Dsonar.tests=/usr/src/. \
  -Dsonar.test.inclusions=**/*.test.js \
- -Dsonar.exclusions=node_modules/*,**/*.test.js,**/*.html,**/*.yml,**/*.yaml,**/*.json,**/mocks/*" \
+ -Dsonar.exclusions=node_modules/**/*,**/*.test.js,**/*.html,**/*.yml,**/*.yaml,**/*.json,**/mocks/*" \
 images.paas.redhat.com/alm/sonar-scanner-alpine:latest -X
 
-#mkdir -p "${WORKSPACE}/artifacts"
+mkdir -p "${WORKSPACE}/artifacts"
 
 # Archive coverage artifacts in Jenkins
-#cp $PWD/coverage* $WORKSPACE/artifacts/.
+cp $PWD/coverage/cover* $PWD/coverage/lcov.info $WORKSPACE/artifacts/
