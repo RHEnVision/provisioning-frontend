7a822e0f86d935db233aba268d99c7680e40cf8a
Ondrej Ezr <oezr@redhat.com>
2023-03-30 12:15:54 +0200

    docs: Human readable azure label

--- src/Components/Common/helpers.js
+++ src/Components/Common/helpers.js
@@ -17,9 +17,11 @@ export const humanizeProvider = (provider) => {
   switch (provider) {
     case AWS_PROVIDER:
       return 'Amazon cloud';
+    case AZURE_PROVIDER:
+      return 'Azure';
     case GCP_PROVIDER:
       return 'Google cloud';
     default:
-      return '';
+      throw new Error(`Unrecognized provider was entered: ${provider}`);
   }
 };
