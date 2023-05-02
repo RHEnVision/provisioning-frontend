117c5097ec32183e6ce0965ace55c7b505067b18
Anna Vitova <avitova@redhat.com>
2023-05-02 13:49:20 +0200

    feat(HMS-1614): Add vCPU limit Warning

--- src/Components/InstanceCounter/index.js
+++ src/Components/InstanceCounter/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { NumberInput } from '@patternfly/react-core';
 import { useWizardContext } from '../Common/WizardContext';
-import { MAX_INSTANCES, MIN_INSTANCES } from './constants';
+import { useQuery } from 'react-query';
+import { MAX_INSTANCES, MIN_INSTANCES, MAX_VCPU } from './constants';
+import { Alert } from '@patternfly/react-core';
+import { fetchInstanceTypesList } from '../../API';
 
 const InstanceCounter = () => {
-  const [{ chosenNumOfInstances }, setWizardContext] = useWizardContext();
+  const [{ chosenInstanceType, chosenNumOfInstances, chosenRegion, chosenSource, provider }, setWizardContext] = useWizardContext();
+  const { data: instanceTypes } = useQuery(['instanceTypes', chosenRegion], () => fetchInstanceTypesList(chosenRegion, provider), {
+    enabled: !!chosenRegion && !!chosenSource && !!chosenInstanceType,
+  });
+  const VCPUAmount = instanceTypes?.find((instanceType) => instanceType.name === chosenInstanceType)?.vcpus;
 
   const onMinus = () => {
     const newValue = chosenNumOfInstances - 1;
@@ -37,18 +44,30 @@ const InstanceCounter = () => {
   };
 
   return (
-    <NumberInput
-      max={MAX_INSTANCES}
-      min={MIN_INSTANCES}
-      value={chosenNumOfInstances}
-      onMinus={onMinus}
-      onChange={onChange}
-      onPlus={onPlus}
-      inputName="instances"
-      inputAriaLabel="number of instances"
-      minusBtnAriaLabel="instances-minus"
-      plusBtnAriaLabel="instances-plus"
-    />
+    <>
+      {VCPUAmount * chosenNumOfInstances > MAX_VCPU && (
+        <Alert
+          data-testid="vcpu_limit_exceeded_alert"
+          ouiaId="vcpu_limit_exceeded_not_supported_alert"
+          variant="warning"
+          isInline
+          title="Warning: Launching many vCPUs which might exceed service quota limit."
+        />
+      )}
+      <NumberInput
+        max={MAX_INSTANCES}
+        min={MIN_INSTANCES}
+        value={chosenNumOfInstances}
+        onMinus={onMinus}
+        onChange={onChange}
+        onPlus={onPlus}
+        inputName="instances"
+        inputAriaLabel="number of instances"
+        minusBtnAriaLabel="instances-minus"
+        plusBtnAriaLabel="instances-plus"
+      />
+    </>
   );
 };
+
 export default InstanceCounter;
