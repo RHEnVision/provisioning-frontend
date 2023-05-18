6641ce42354a92cc3aaac547aae9c905d72be626
Amir Fefer <amirfefer@gmail.com>
2023-05-18 11:39:08 +0300

    feat(HMS-1907): add dev script for  containerizing provisioning env

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,7 +1,8 @@
 const LOCAL_API_PORT = 8000;
+const PROV_API = process.env.PROV_API_HOST || 'localhost';
 
 module.exports = {
   routes: {
-    '/api/provisioning': { host: `http://localhost:${LOCAL_API_PORT}` },
+    '/api/provisioning': { host: `http://${PROV_API}:${LOCAL_API_PORT}` },
   },
 };
