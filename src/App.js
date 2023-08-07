67cb0c4bbd14239ef3e9524c143c799c256ab39a
Amir Fefer <amirfefer@gmail.com>
2023-08-07 18:10:46 +0300

    build: remove sample page and unused packages

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import React, { Fragment, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Routes } from './Routes';
+import React, { Fragment } from 'react';
 import './App.scss';
 
-import { getRegistry } from '@redhat-cloud-services/frontend-components-utilities/Registry';
-import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
-import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
-import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
-
-const App = (props) => {
-  const history = useHistory();
-  const { on: onChromeEvent } = useChrome();
-
-  useEffect(() => {
-    const registry = getRegistry();
-    const unregister = onChromeEvent('APP_NAVIGATION', (event) => history.push(`/${event.navId}`));
-    registry.register({ notifications: notificationsReducer });
-
-    return () => {
-      unregister?.();
-    };
-  }, []);
-
-  return (
-    <Fragment>
-      <NotificationsPortal />
-      <Routes childProps={props} />
-    </Fragment>
-  );
+const App = () => {
+  return <Fragment></Fragment>;
 };
 
 export default App;
